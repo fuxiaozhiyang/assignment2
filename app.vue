3c489a87949175b18c3465975573456ddeb78d06
Xinyang Wen <927597@ACDM.DS.SAIT.CA>
2024-01-25 15:48:05 -0700

    app-styles

--- app.vue
+++ app.vue
@@ -4,3 +4,35 @@
     <NuxtPage />
   </div>
 </template>
+
+<style>
+header {
+  background-color: #12194A;
+  color: #FFFFFF;
+  padding: 10px;
+  text-align: center;
+}
+
+html{
+  background-color: #393E8F;
+}
+
+h3{
+  color: #EFBEB7;
+  text-align: center;
+}
+
+h4{
+  color: #F3CC64;
+  text-align: center;
+}
+
+.aligncenter {
+    text-align: center;
+}
+
+.border{
+  border: 10px #B12228;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,13 +5,17 @@ const characters = data;
 <template>
   <main>
     <header>
-      <h1>Disney Character Gallery</h1>
+      <h1>Disney Character Gallery! <br> All of the Characters You Didn't know that was part of Disney!</h1>
     </header>
-    <section>
+       <section>
         <ul>
         <li v-for="character in characters.data" :key="character.id">
           <h3>{{ character.name }}</h3>
-          <img :src="character.imageUrl" />
+          <p class="aligncenter">
+            <span class="border">
+           <img :src="character.imageUrl" />
+            </span>
+          </p>
           <h4>{{"Films:" + character.films }}</h4>
         </li>
       </ul>
