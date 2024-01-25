9fcb4132c76e0feccf9932cb93c7c0b4c02014d8
Xinyang Wen <927597@ACDM.DS.SAIT.CA>
2024-01-25 14:52:09 -0700

    index-disney

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup>
+const { data } = await useFetch("/api/disney");
+const characters = data;
+</script>
+<template>
+  <main>
+    <header>
+      <h1>Disney Character Gallery</h1>
+    </header>
+    <section>
+        <ul>
+        <li v-for="character in characters.data" :key="character.id">
+          <h3>{{ character.name }}</h3>
+          <img :src="character.imageUrl" />
+          <h4>{{"Films:" + character.films }}</h4>
+        </li>
+      </ul>
+    </section>
+  </main>
+</template>
